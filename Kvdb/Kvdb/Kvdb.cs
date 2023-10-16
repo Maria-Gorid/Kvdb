134750b2f55769a42cb686f201aed959184701b2
Андрей Боев <a.boev.it@gmail.com>
2023-10-17 01:30:04 +0300

    Исправление XML сериализации

--- Kvdb/Kvdb/Kvdb.cs
+++ Kvdb/Kvdb/Kvdb.cs
@@ -1,12 +1,16 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace Kvdb
 {
+    using DataBase = Dictionary<string, Dictionary<string, string>>;
     public class Kvdb
     {
-        static Dictionary<string,Dictionary<string,string>> db =new Dictionary<string,Dictionary<string,string>>();
+        static DataBase? db = new DataBase();
 
         public static void Parse(string command)
         {
@@ -14,61 +18,32 @@ namespace Kvdb
             switch (param[0])
             {
                 case "put":
-                    if (param.Length == 3 )
+                    if (param.Length == 3)
                     {
-                        if (db[""].ContainsKey(param[1]))
-                        {
-                            Console.WriteLine(db[""][param[1]]);
-                        }
-                        else
-                        {
-                            Console.WriteLine("");// переписать
-                        }
+                        Console.WriteLine($"{(db != null && db[""].ContainsKey(param[1]) ? db[""][param[1]] : "")}");
                         db[""][param[1]] = param[2];
                     }
 
                     if (param.Length == 4)
                     {
-                        if (db.ContainsKey(param[1]))
-                        {
-                            if (db[param[1]].ContainsKey(param[2]))
-                            {
-                                Console.WriteLine(db[param[1]][param[2]]);
-                            }
-                            else
-                            {
-                                Console.WriteLine("");// переписать
-                            }
-                            db[param[1]][param[2]] = param[3];
-                        }
+                        Console.WriteLine(
+                            $"{(db != null && db[param[1]].ContainsKey(param[2]) ? db[param[1]][param[2]] : "")}");
+                        db[param[1]][param[2]] = param[3];
                     }
-                    
+
                     break;
                 case "get":
                     if (param.Length == 2)
                     {
-                        if (db[""].ContainsKey(param[1]))
-                        {
-                            Console.WriteLine(db[""][param[1]]);
-                        }
-                        else
-                        {
-                            Console.WriteLine("");
-                        }
-                        
+                        Console.WriteLine($"{(db != null && db[""].ContainsKey(param[1]) ? db[""][param[1]] : "")}");
                     }
+
                     if (param.Length == 3)
                     {
-                        if (db[param[1]].ContainsKey(param[2]))
-                        {
-                            Console.WriteLine(db[param[1]][param[2]]);
-                        }
-                        else
-                        {
-                            Console.WriteLine("");
-                        }
-                        
+                        Console.WriteLine(
+                            $"{(db != null && db[param[1]].ContainsKey(param[2]) ? db[param[1]][param[2]] : "")}");
                     }
+
                     break;
                 case "remove":
                     if (param.Length == 2)
@@ -82,8 +57,8 @@ namespace Kvdb
                         {
                             Console.WriteLine("");
                         }
-                        
                     }
+
                     if (param.Length == 3)
                     {
                         if (db[param[1]].ContainsKey(param[2]))
@@ -95,40 +70,45 @@ namespace Kvdb
                         {
                             Console.WriteLine("");
                         }
-                        
                     }
+
                     break;
             }
         }
 
         public static void Deserialize()
         {
-            XmlSerializer formatter = new XmlSerializer(typeof(Dictionary<string, Dictionary<string, string>>));
-            FileStream file = new FileStream("data.db", FileMode.OpenOrCreate);
-            db = formatter.Deserialize(file) as Dictionary<string, Dictionary<string, string>> ;
-            if (db == null)
+            if (!File.Exists("data.xml"))
             {
-                db = new Dictionary<string, Dictionary<string, string>>();
+                db = new DataBase();
                 db[""] = new Dictionary<string, string>();
-                
+                return;
             }
+
+            DataContractSerializer serializer = new DataContractSerializer(typeof(DataBase));
+            FileStream file = new FileStream("data.xml", FileMode.OpenOrCreate);
+            var xmlReader = XmlDictionaryReader.CreateTextReader(file, new XmlDictionaryReaderQuotas());
+            db = serializer.ReadObject(xmlReader) as DataBase;
             file.Close();
+            xmlReader.Close();
         }
+
         public static void Serialize()
         {
-            XmlSerializer formatter = new XmlSerializer(typeof(Dictionary<string, Dictionary<string, string>>));
-            FileStream file = new FileStream("data.db", FileMode.OpenOrCreate);
-            formatter.Serialize(file, db);
+            DataContractSerializer serializer = new DataContractSerializer(typeof(DataBase));
+            FileStream file = new FileStream("data.xml", FileMode.Create);
+            var xmlWriter = XmlDictionaryWriter.CreateTextWriter(file);
+            serializer.WriteObject(xmlWriter, db);
+            xmlWriter.Close();
             file.Close();
         }
-        
+
         public static void Main(string[] args)
         {
             Deserialize();
-            string input = Console.ReadLine();
+            string input = Console.ReadLine()!;
             Parse(input);
             Serialize();
-            
         }
     }
 }
